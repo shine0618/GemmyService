5a4bbda5f2d2b51e68364f8c951caca53335a977
唐文杰 <shine0618@outlook.com>
2021-04-29 09:21:03 +0800

    w

---
+++ 1GemmyModel/Migrations/202104290120236_init33332.cs
@@ -0,0 +1,16 @@
+namespace _1GemmyModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init33332 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
