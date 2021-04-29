fe32ee6342a71a1e134898529a7e2d8c06ffd18e
唐文杰 <shine0618@outlook.com>
2021-04-29 14:38:28 +0800

    导入ui样式

---
+++ GemmyService/Controllers/JCSelectionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GemmyService.Controllers
+{
+    public class JCSelectionController : Controller
+    {
+        // GET: JCSelection
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult main()
+        {
+            return View();
+        }
+        public ActionResult selectType()
+        {
+            return View();
+        }
+        public ActionResult Eservice()
+        {
+            return View();
+
+        }
+
+        public ActionResult office_Eservice()
+        {
+
+            return View();
+        }
+
+        public ActionResult office()
+        {
+            return View();
+
+        }
+    }
+}
