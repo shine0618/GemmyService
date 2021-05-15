7babf99e049161478af02ec6c84d3f1bfe404450
唐文杰 <shine0618@outlook.com>
2021-05-15 13:43:31 +0800

    34

---
+++ 1GemmyModel/Migrations/202105150543032_init3456543.cs
@@ -0,0 +1,18 @@
+﻿namespace _1GemmyModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init3456543 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.T_Product_office_desk_detail", "configurationNo", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.T_Product_office_desk_detail", "configurationNo");
+        }
+    }
+}
