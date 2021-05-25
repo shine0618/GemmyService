58102292665fe068b41f55ab24c54b71009afe1e
唐文杰 <shine0618@outlook.com>
2021-05-25 16:12:35 +0800

    23432

---
+++ 1GemmyModel/Migrations/202105250811339_initajsgas.cs
@@ -0,0 +1,49 @@
+﻿namespace _1GemmyModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initajsgas : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.T_Office_Files",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        partType = c.String(),
+                        Mode = c.String(),
+                        FileName = c.String(),
+                        Nature = c.String(),
+                        Information = c.String(),
+                        Path = c.String(),
+                        Size = c.Long(nullable: false),
+                        Createdate = c.DateTime(nullable: false),
+                        Modifydate = c.DateTime(),
+                        Outdate = c.DateTime(),
+                        Person = c.String(),
+                        Type = c.String(),
+                        Permission = c.String(),
+                        Products = c.String(),
+                        Lock = c.Boolean(nullable: false),
+                        Language = c.String(),
+                        verificationCode = c.String(),
+                        deleteSign = c.Int(),
+                        UpdateTime = c.DateTime(),
+                        CreateTime = c.DateTime(),
+                        deletePerson = c.String(),
+                        CreatePerson = c.String(),
+                        UpdatePerson = c.String(),
+                        Remark = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.T_Office_Files");
+        }
+    }
+}
---
+++ 1GemmyModel/Model/ModelProductOffice/T_Office_Files.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1GemmyModel.Model.ModelProductOffice
+{
+   public class T_Office_Files:T_Base
+    {
+
+
+        /// <summary>
+        /// 关联的类型 比如说是地脚的Mode  还是框架的Mode
+        /// </summary>
+        public string partType { get; set; }
+        public string Mode { get; set; }
+         
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        public string FileName { get; set; }
+        /// <summary>
+        /// 文件种类
+        /// </summary>
+        public string Nature { get; set; }
+        /// <summary>
+        /// 文件信息
+        /// </summary>
+        public string Information { get; set; }
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string Path { get; set; }
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public long Size { get; set; }
+        /// <summary>
+        /// 创建日期
+        /// </summary>
+        public DateTime Createdate { get; set; }
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        public DateTime? Modifydate { get; set; }
+        /// <summary>
+        /// 失效日期
+        /// </summary>
+        public DateTime? Outdate { get; set; }
+        /// <summary>
+        /// 添加者名称
+        /// </summary>
+        public string Person { get; set; }
+        /// <summary>
+        /// 文件后缀名
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// 允许查看
+        /// </summary>
+        public string Permission { get; set; }
+        /// <summary>
+        /// 关联产品
+        /// </summary>
+        public string Products { get; set; }
+        /// <summary>
+        /// 是否被锁定
+        /// </summary>
+        public bool Lock { get; set; }
+        /// <summary>
+        /// 语言版本
+        /// </summary>
+        public string Language { get; set; }
+    }
+}
