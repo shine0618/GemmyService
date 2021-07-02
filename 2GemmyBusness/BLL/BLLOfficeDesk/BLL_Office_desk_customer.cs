8d78d40661cbc47bf0387924c06e10d7bb9d82ea
唐文杰 <shine0618@outlook.com>
2021-07-02 10:50:09 +0800

    34

--- 2GemmyBusness/BLL/BLLOfficeDesk/BLL_Office_desk_customer.cs
+++ 2GemmyBusness/BLL/BLLOfficeDesk/BLL_Office_desk_customer.cs
@@ -9,7 +9,18 @@ namespace _2GemmyBusness.BLL.BLLOfficeDesk
 {
   public  class BLL_Office_desk_customer:BLLBase
     {
-        
+
+        public List<T_Product_office_desk_customer> GetT_Product_office_desk_customer(string langCode, string userName)
+        {
+            var query = from x in read_db.T_Product_office_desk_customer
+                        where x.deleteSign != 1
+                        where x.customerUserName == userName
+                        select x;
+
+            return query.ToList();
+
+        }
+
         public int AddT_Product_office_desk_customer(T_Product_office_desk_customer model)
         {
            return  base.AddEntities<T_Product_office_desk_customer>(model);
---
+++ GemmyService/Controllers/JCSelection_CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using _1GemmyModel.Model.ModelProductOffice;
+using _2GemmyBusness.BLL.BLLOfficeDesk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GemmyService.Views
+{
+    public class JCSelection_CustomerController : Controller
+    {
+        // GET: JCSelection_Customer
+   
+        public ActionResult personal(string personal,string token)
+        {
+
+            //如果语言是默认的话
+            if (Session["PageLanguage"] == null)
+            {
+                Session["PageLanguage"] = "default";
+            }
+            if (Session["emailName"] == null)
+            {
+                Session["emailName"] = "";
+            }
+
+
+            return View();
+        }
+
+        BLL_Office_desk_customer bll = new BLL_Office_desk_customer();
+
+        [HttpGet]
+        public JsonResult GetCustomerList(string langCode,string userName,string token)
+        {
+            List<T_Product_office_desk_customer> list = bll.GetT_Product_office_desk_customer(langCode, userName);
+            JsonResult jr = Json(list, JsonRequestBehavior.AllowGet);
+            jr.MaxJsonLength = int.MaxValue;
+            return jr;
+        }
+    }
+}
